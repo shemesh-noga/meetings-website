671d7ce2dff105049e5da8cc8cc67aac41d6e006
tair18oz <tairoz@hilma.tech>
2024-10-13 18:37:25 +0300

    db

--- js/DB.js
+++ js/DB.js
@@ -31,19 +31,67 @@ let meetings=[
 
 ];
 
+
+window.localStorage.setItem("users", JSON.stringify(users));
+window.localStorage.setItem("meetings", JSON.stringify(meetings));
+
+//מחלקת משתמש חדש
 class User{
     constructor(username, password){
-        this.username=username;
-        this.password=password;
-        this.meetings=[];
+        this.username = username;
+        this.password = password;
+        this.meetings = [];
+    }
+}
+
+// מחלקת פגישה חדשה
+class Meeting{
+    constructor(name, time){
+        this.name = name;
+        this.time = time;
+        this.id = JSON.parse(window.localStorage.getItem("meetings")).length + 1;
+    }
+}
+
+// שליפת מידע
+function getInfo(key, index){
+    const value = JSON.parse(window.localStorage.getItem(key))
+    return value;
+}
+
+
+// הוספת פגישה חדשה
+function addNewMeeting(name, time){
+    const meetings=JSON.parse(window.localStorage.getItem("meetings"));
+    const currentUser=JSON.parse(window.localStorage.getItem("currentUser"));
+    const users=JSON.parse(window.localStorage.getItem("users"));
+
+    const newMeeting = new Meeting(name, time);
+
+
+    meetings.push(newMeeting); 
+    window.localStorage.setItem("meetings",JSON.stringify(meetings));
+
+    currentUser["meetings"].push(newMeeting.id);
+    window.localStorage.setItem("currentUser", JSON.stringify(currentUser));
+
+    for(let i = 0 ; i < users.length ; i ++) {
+        if(users[i]["username"] === currentUser["username"]) {
+            users[i]["meetings"].push(newMeeting.id)
+            window.localStorage.setItem("users", JSON.stringify(users))
+            break;
+        }
     }
 }
 
 
-function checkSignUp(username, password){
+
+// בודק האם קיים משתמש בשם הזה, אם לא מוסיף את המשתמש החדש
+function addNewUser(username, password){
     const users=JSON.parse(window.localStorage.getItem("users"));
     for(let i=0; i<users.length; i++){
         if(users[i]["username"]===username){
+            console.log("there is already a user with this name")
             return /*  */;
         }
     }
@@ -55,6 +103,40 @@ function checkSignUp(username, password){
 }
 
 
+function deleteMeeting(id){
+    const meetings=JSON.parse(window.localStorage.getItem("meetings"));
+    const currentUser=JSON.parse(window.localStorage.getItem("currentUser"));
+    const users=JSON.parse(window.localStorage.getItem("users"));
+
+   for(let i = 0 ; i < meetings.length ; i ++){
+        if(meetings[i].id === id) {
+            meetings[i] = "";
+            window.localStorage.setItem("meetings",JSON.stringify(meetings));
+            break;
+        }
+   }
+
+   for(let i = 0 ; i < currentUser["meetings"] ; i++){
+        if(currentUser["meetings"][i] === id) {
+            currentUser["meetings"].splice(i, 1);
+            window.localStorage.setItem("currentUser", JSON.stringify(currentUser))
+            break;
+        }
+   }
+
+   currentUserToUsers(currentUser);
+}
+
+
+//מעדכן את הפגישה
+function updateMeeting(id, key, value){
+    const meetings=JSON.parse(window.localStorage.getItem("meetings"));
+    for(let i = 0 ; i < meetings.length ; i ++){
+        if(meetings[i]["id"] === id) {
+            meetings[key] = value;
+        }
+    }
+}
 
 
 
@@ -72,15 +154,20 @@ function checkSignUp(username, password){
 
 
 
+// פונקציות עזר
 
+// פונקציה שמעדכנת את תוכן משתמש נוכחי למערך היוזרים
+function currentUserToUsers(currentUser) {
+    const users=JSON.parse(window.localStorage.getItem("users"));
 
+    for(let i = 0 ; i < users.length ; i++) {
+        if(users[i]["username"] === currentUser["username"]) {
+            users[i] = currentUser;
+            window.localStorage.setItem("users", users)
+        }
+    }
 
-
-
-
-
-
-
+}
 
 
 
