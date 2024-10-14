ce5e8a7a84e7be51e1fa58e718ac2b121e495b15
tair18oz <tairoz@hilma.tech>
2024-10-14 18:02:26 +0300

    ttt

--- js/fajax.js
+++ js/fajax.js
@@ -13,46 +13,9 @@ class Fajax{
   }
 }
 
-
-
-
-
-
 const fajax= new Fajax();
 // fajax.onload = function() {
    
 //   }
 fajax.open("GET", "hi/hello");
 fajax.send();
-
-
-
-//build the "open" function by enter all the parameters to data and send it to the server
-// function send(method, url, data = ""){
-
-//     obj = {
-//         "method": method ,
-//          "url": url ,
-//           "data":  data
-//         }
-//      JSON.stringify(objMethod);
-     
-//     server(obj);
-// }
-
- function onload (){
-
- }
-
- function response(){
-
- }
-
-
-
-
-
- xhttp.open("GET", "ajax_info.txt");
-
-
-
